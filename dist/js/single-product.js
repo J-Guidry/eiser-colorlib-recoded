7f7332cde1fc2a9286b1f1bd4ebe7cab5e9af174
Guidry <jh_guidry@outlook.com>
2020-01-30 11:29:56 -0600

    tab system added with arrow keys and tab sky to switch as needed and styling done for entire section

--- dist/js/single-product.js
+++ dist/js/single-product.js
@@ -2,6 +2,9 @@ const thumbnails = document.querySelectorAll(".thumbnail");
 const increaseArrow = document.querySelector(".increase");
 const decreaseArrow = document.querySelector(".decrease");
 const quantity = document.querySelector(".quantity-input");
+const tablinks = document.querySelector(".tab-links");
+const tabs = tablinks.querySelectorAll("a");
+const panels = document.querySelectorAll(".panel");
 
 const slider = tns({
     container: ".image-carousel",
@@ -36,4 +39,46 @@ decreaseArrow.addEventListener("click", ()=> {
   if(quantity.value > 0){
     quantity.value--;
   }
+});
+
+// activate panels and change aria attributes when switching tabs
+function switchTab(oldTab, chosenTab) {
+  chosenTab.focus();
+  chosenTab.setAttribute("aria-selected", "true");
+  chosenTab.classList.toggle("tab-active");
+  oldTab.setAttribute("aria-selected", 'false');
+  oldTab.classList.toggle("tab-active");
+  let index = Array.prototype.indexOf.call(tabs,chosenTab);
+  let oldIndex = Array.prototype.indexOf.call(tabs,oldTab);
+  panels[oldIndex].classList.toggle("panel-active");
+  panels[oldIndex].setAttribute("aria-hidden", "true");
+  panels[index].classList.toggle("panel-active");
+  panels[index].setAttribute("aria-hidden", "false");
+}
+
+// add click and keyboard event listeners to tabs
+tabs.forEach(function(tab, i) {
+  tab.addEventListener("click", function(event) {
+      event.preventDefault();
+      const currentlySelectedTab = document.querySelector('[aria-selected="true"]');
+      const newlySelectedTab = event.currentTarget;
+      if (currentlySelectedTab !== newlySelectedTab) {
+          switchTab(currentlySelectedTab, newlySelectedTab);
+      }
+  });
+  
+  tab.addEventListener('keydown', e => {  
+      let index = Array.prototype.indexOf.call(tabs, e.currentTarget);
+      let dir = e.which === 37 ? index - 1 
+              : e.which === 39 ? index + 1 
+              : e.which === 40 ? 'down' : null;
+
+      if (dir !== null) {
+          e.preventDefault();
+          dir === 'down' ? panels[i].focus() 
+              : tabs[dir] ? switchTab(e.currentTarget, tabs[dir]) 
+              : void 0;
+      }
+  });
+
 });
