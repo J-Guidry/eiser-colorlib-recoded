e0e70e47347f610ab1194f10a5d072799d648a5e
Guidry <jh_guidry@outlook.com>
2020-02-13 12:17:43 -0600

    $ added to price slider output

--- dist/js/selected.js
+++ dist/js/selected.js
@@ -12,5 +12,5 @@ output.textContent += slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.textContent = this.value;
+  output.textContent = "$ " + this.value;
 }
